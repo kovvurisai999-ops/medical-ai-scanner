aa56fa88f01f8db370fb84c7f56163c67fbef9a9
kovvuri sai <kovvurisai999@gamil.com>
2026-05-09 15:13:47 +0530

    Configure Render to create dummy models for deployment stability

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Create lightweight dummy models to prevent deployment crashes
+python create_dummy_models.py
+
 python manage.py collectstatic --no-input
 python manage.py migrate
